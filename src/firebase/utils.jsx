26dbabbb4410c52997d08181f54cd4167f76270c
Sepideh Attar <sepideh.att@gmail.com>
2024-02-06 14:50:27 +0100

    Added firebase config for login with google account.

---
+++ src/firebase/utils.jsx
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import * as auth from 'firebase/auth';
+import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
+
+const GoogleProvider = new auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () =>
+    firebase.auth.signInWithPopup(GoogleProvider);
+
+export default firebase; // Add this line to export firebase as the default export
