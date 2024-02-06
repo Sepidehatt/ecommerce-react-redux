1f8ff02a8a7db513bddbc35068db4984447e7eb5
Sepideh Attar <sepideh.att@gmail.com>
2024-02-06 14:48:47 +0100

    Added main and homepage layouts

---
+++ src/layouts/HomePageLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import Footer from '../components/Footer/Footer';
+
+const HomePageLayout = (props) => {
+    return (
+        <div className="full-height">
+            <Header />
+            {props.children}
+            <Footer />
+        </div>
+    );
+};
+
+export default HomePageLayout;
